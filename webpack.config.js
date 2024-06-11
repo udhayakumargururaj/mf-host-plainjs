0f32e8a6683340221b9ad608355a74375806ecda
udhayakumar gururaj <udhayakumargururaj@gmail.com>
2024-06-11 18:02:56 +0530

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,44 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { ModuleFederationPlugin } = require('webpack').container;
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const path = require('path');
 
 module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
   devServer: {
-    port: 3000,
     contentBase: path.join(__dirname, 'dist'),
+    port: 5000,
+  },
+  output: {
+    publicPath: 'http://localhost:5000/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
     new ModuleFederationPlugin({
-      name: 'host', // Name of the host application
-      filename: 'remoteEntry.js',
+      name: 'hostApp',
       remotes: {
-        remoteApp: 'remoteApp@http://localhost:3001/remoteEntry.js', // Name and URL of the remote application
+        remoteApp: 'remoteApp@http://localhost:3001/remoteEntry.js',
       },
       shared: {
-        react: { singleton: true,requiredVersion: '^17.0.2' },
-        'react-dom': { singleton: true,requiredVersion: '^17.0.2' },
+        react: {
+          singleton: true,
+          requiredVersion: '^17.0.2',
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: '^17.0.2',
+        },
       },
     }),
   ],
